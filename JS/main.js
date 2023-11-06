6ec30ffbac7791467d56226c7820e3c53f4b75bb
dieromeo <diegoromeo99@icloud.com>
2023-11-06 15:10:50 +0100

    feat: scaffolding

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+/* 
+# Calcolatore prezzo del biglietto del treno
+
+Effettuare dei prompt che chiedano all'utente:
+
+1) Numero di chilometri da percorrere.
+2) Età del passeggero.
+
+Entrambe saranno da inserire all'interno di delle variabili.
+
+### Calcolo prezzo
+
+Definire un a variabile che calcoli il prezzo del biglietto moltiplicando i km inseriti dall'utente per il prezzo al km (0,21€).
+
+### Funzione sconto
+
+In una fuzione calcolare il prezzo aggiornato del biglietto a seconda dell'età nel seguente modo:
+
+- Se l'età è minore di 18 
+    - allora il prezzo è scontato del 20%.
+
+- Se l'età e maggiore o uguale a 65
+    - allora il prezo scontato è del 40%.
+
+- Altrimenti non viene effettuato nessuno sconto.
+
+Indicare il prezzo finale con al massimo due numeri decimali. */

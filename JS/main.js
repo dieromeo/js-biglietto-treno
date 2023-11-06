3253106237bf61433ed2e8fbe0bad57b54f46f76
dieromeo <diegoromeo99@icloud.com>
2023-11-06 15:24:38 +0100

    feat: assegnazione variabili

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,28 @@
-/* 
-# Calcolatore prezzo del biglietto del treno
-
+"use strict";
+/*
 Effettuare dei prompt che chiedano all'utente:
 
 1) Numero di chilometri da percorrere.
 2) Età del passeggero.
 
-Entrambe saranno da inserire all'interno di delle variabili.
+Entrambe saranno da inserire all'interno di delle variabili. */
 
+const km = parseInt(prompt("Inserire il numero di km da percorrere."));
+console.log(km);
+
+const etaUtente = parseInt(prompt("Inserisci la tua età"));
+console.log(etaUtente);
+
+
+/*
 ### Calcolo prezzo
 
-Definire un a variabile che calcoli il prezzo del biglietto moltiplicando i km inseriti dall'utente per il prezzo al km (0,21€).
+Definire un a variabile che calcoli il prezzo del biglietto moltiplicando i km inseriti dall'utente per il prezzo al km (0,21€).*/
 
+var prezzoBiglietto = km * 0.21;
+console.log(prezzoBiglietto + '€');
+
+/*
 ### Funzione sconto
 
 In una fuzione calcolare il prezzo aggiornato del biglietto a seconda dell'età nel seguente modo:
@@ -22,6 +33,9 @@ In una fuzione calcolare il prezzo aggiornato del biglietto a seconda dell'età 
 - Se l'età e maggiore o uguale a 65
     - allora il prezo scontato è del 40%.
 
-- Altrimenti non viene effettuato nessuno sconto.
+- Altrimenti non viene effettuato nessuno sconto.*/
 
+
+
+/*
 Indicare il prezzo finale con al massimo due numeri decimali. */
